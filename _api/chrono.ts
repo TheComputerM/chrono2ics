e5a9b334244c40af856401e8f9d0012f53c473e1
TheComputerM <mudit.somani00@gmail.com>
2025-08-05 01:07:25 +0530

    chore: add biomejs

--- _api/chrono.ts
+++ _api/chrono.ts
@@ -1,12 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
- 
-export default function handler(
-  request: VercelRequest,
-  response: VercelResponse,
-) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
+export async function POST(request: Request) {
+  const { id } = await request.json();
+  return new Response(`ID is ${id}`);
 }
