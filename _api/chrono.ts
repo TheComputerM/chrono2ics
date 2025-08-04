9618b6be323708fd7104ba93a598b9e305d0547b
TheComputerM <mudit.somani00@gmail.com>
2025-08-05 01:15:37 +0530

    fix: add cors headers

--- _api/chrono.ts
+++ _api/chrono.ts
@@ -1,3 +1,10 @@
+export const headers = {
+  "Access-Control-Allow-Credentials": "true",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+  "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+}
+
 export async function POST(request: Request) {
 	const { id } = await request.json();
 	return await fetch(`https://www.chrono.crux-bphc.com/api/timetable/${id}`);
