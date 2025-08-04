1cc58e7b4e91cabffc5bdd85e6d8ccd5ab4eeeb1
TheComputerM <mudit.somani00@gmail.com>
2025-08-05 01:20:14 +0530

    fix: parse and create a new response

--- _api/chrono.ts
+++ _api/chrono.ts
@@ -1,11 +1,6 @@
-export const headers = {
-  "Access-Control-Allow-Credentials": "true",
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-  "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-}
-
 export async function POST(request: Request) {
 	const { id } = await request.json();
-	return await fetch(`https://www.chrono.crux-bphc.com/api/timetable/${id}`);
+	const response = await fetch(`https://www.chrono.crux-bphc.com/api/timetable/${id}`);
+  const data = await response.json();
+  return Response.json(data);
 }
